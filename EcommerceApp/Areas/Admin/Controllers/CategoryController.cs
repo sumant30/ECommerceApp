cedb0950cd3661c4d6d73aefa3a8b4f8aedc1c98
Sumant Natkar <sumant30@gmail.com>
2017-07-02 11:14:05 +0530

    Added layout pages & create plus index pages  in app.

--- EcommerceApp/Areas/Admin/Controllers/CategoryController.cs
+++ EcommerceApp/Areas/Admin/Controllers/CategoryController.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace EcommerceApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Route("Product/[action]")]
-    public class ProductController : Controller
+    [Route("Category/[action]")]
+    public class CategoryController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
